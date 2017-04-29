b496ba6179046484712aba7a781d759cc27e3737
Connor Blandin <connor_blandin@baylor.edu>
2017-04-29 14:12:45 -0500

    Color changing

--- main.cpp
+++ main.cpp
@@ -47,19 +47,9 @@ int main(int argc, char ** argv)
     Graph graph(g, font, n, origin, graphSize);
     graph.setNLoc(Point(1000, 650));
 
-        Font f(25);
 
 
-    f.drawString(g, Point(700, 500), "this is a test(");
 
-    f.setColor(COLOR::GREEN);
-    f.drawString(g, Point(700, 600), "this is a test");
-
-
-    bool equals = Point(0,0) == Point(0,0);
-    Point(350,350).draw(g);
-
-    cout << equals << endl;
 
     g.update();
 
